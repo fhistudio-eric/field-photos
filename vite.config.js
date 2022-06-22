326e2173bf3386bb3d6c1c813b22a4d384c68935
Eric <esmith@fhistudio.com>
2022-06-22 13:13:40 -0400

    config changes for proxy

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: "",
-  // server: {
-  //   origin: "http://localhost:3000",
-  //   proxy: {
-  //     '/phpmm': {
-  //       // target: 'https://materials.fhistudio.com/',
-  //       target: 'https://www.fhistudio-apps.com/',
-  //       changeOrigin: true,
-  //       secure: true,
-  //       ws: false,
-  //     }
-  //   }
-  // },
+  base: "https://www.fhistudio-apps.com/fieldphotos",
+  // base: "",
+  server: {
+    origin: "http://localhost:3000",
+    proxy: {
+      '/php': {
+        target: 'https://www.fhistudio-apps.com/fieldphotos',
+        changeOrigin: true,
+        secure: false,
+        ws: false,
+      }
+    }
+  },
 })
