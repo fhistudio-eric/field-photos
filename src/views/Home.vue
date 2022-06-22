d0cc7af75a4cb67ef32398ac68e325f393f216ee
Eric <esmith@fhistudio.com>
2022-06-22 13:14:15 -0400

    hard coded url for axios - need to investigate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="bg-white h-full p-6 flex flex-col justify-between">
-    <div class="flex flex-col">
-      <div class="text-3xl font-extrabold mb-4 flex items-center">
+  <div class="bg-white h-full p-4 flex flex-col justify-between">
+    <div class="flex flex-col bg-white shadow-md rounded-sm p-4">
+      <div class="text-3xl font-extrabold mb-10 flex items-center">
         <PhotographIcon class="text-gray-900 mr-1 w-10 h-10" />
-        Field Photos
+        <div
+          class="
+            font-extrabold
+            text-transparent text-4xl
+            bg-clip-text bg-gradient-to-r
+            from-blue-400
+            to-green-600
+          "
+        >
+          Field Photos
+        </div>
       </div>
       <div class="text-lg font-bold">Your Name: *</div>
       <div class="w-full">
@@ -19,7 +29,23 @@
           type="text"
           class="w-full border border-gray-500 p-2 rounded-sm"
           v-model="store.sessionName"
+          @focusout="begin"
         />
+        <div
+          v-if="sessionExists"
+          class="bg-red-200 mt-1 flex flex-col shadow-md"
+        >
+          <div class="mr-1 flex w-full font-bold bg-red-300 p-2">
+            <ExclamationIcon class="text-gray-900 mr-1 w-6" />
+            <div>Warning - Session Name Exists</div>
+          </div>
+          <div class="p-3">
+            A session with this name already exists. You will be adding photos
+            to an existing session. Disregard if you were provided with a
+            session name.
+          </div>
+          <div class="p-3">To create a unique session enter a unique name.</div>
+        </div>
       </div>
       <div class="text-lg font-bold mt-2">Session notes:</div>
       <div class="w-full">
@@ -31,30 +57,33 @@
     </div>
     <div class="w-full flex text-center flex-col">
       <span class="mb-2 text-gray-500">* required to begin</span>
-      <button
-        class="
-          w-full
-          border border-gray-500
-          p-4
-          rounded-sm
-          text-xl
-          disabled:opacity-30
-          transition-all
-          duration-400
-        "
-        :disabled="
-          store.sessionName == '' ||
-          store.userName == '' ||
-          store.sessionName == null ||
-          store.userName == null ||
-          store.sessionName == undefined ||
-          store.userName == undefined
-            ? true
-            : false
-        "
-      >
-        <router-link to="photo"> Begin </router-link>
-      </button>
+      <router-link to="photo">
+        <button
+          class="
+            w-full
+            border border-gray-500
+            p-4
+            rounded-sm
+            text-xl
+            disabled:opacity-30
+            transition-all
+            duration-400
+            bg-white
+          "
+          :disabled="
+            store.sessionName == '' ||
+            store.userName == '' ||
+            store.sessionName == null ||
+            store.userName == null ||
+            store.sessionName == undefined ||
+            store.userName == undefined
+              ? true
+              : false
+          "
+        >
+          Begin
+        </button>
+      </router-link>
     </div>
   </div>
 </template>
@@ -63,14 +92,30 @@
 import { onMounted, ref, watch } from "vue";
 import { defaultStore } from "../store";
 import { useRouter, useRoute } from "vue-router";
+import axios from "axios";
 
 import { PhotographIcon } from "@heroicons/vue/outline";
+import { ExclamationIcon } from "@heroicons/vue/outline";
+
+const store = defaultStore();
+const router = useRouter();
+const route = useRoute();
 
 onMounted(() => {
   store.sessionDate = new Date().toISOString().split("T")[0];
 });
 
-const store = defaultStore();
-const router = useRouter();
-const route = useRoute();
+const sessionExists = ref(false);
+
+const begin = async () => {
+  let formData = new FormData();
+  formData.append("sessionName", store.sessionName);
+
+  const response = await axios.post("https://www.fhistudio-apps.com/fieldphotos/php/checkSessionName.php", formData);
+  if (response.data == "Found") {
+    sessionExists.value = true;
+  } else {
+    sessionExists.value = false;
+  }
+};
 </script>
--- src/views/PhotoUploader.vue
+++ src/views/PhotoUploader.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="flex flex-col h-1/2">
+  <div class="flex flex-col h-2/3 overflow-hidden relative">
+    <div
+      class="
+        absolute
+        top-0
+        w-full
+        p-2
+        flex
+        items-center
+        justify-center
+        transition-all
+        duration-300
+        z-50
+      "
+      :class="{ '-mt-20': uploadSuccess == false }"
+    >
+      <div
+        class="p-4 px-8 rounded-full text-white text-2xl"
+        :class="{
+          'bg-green-600': uploadStatus == 'Success',
+          'bg-red-600': uploadStatus != 'Success',
+        }"
+      >
+        <div v-if="uploadStatus == 'Success'" class="flex items-center">
+          <BadgeCheckIcon class="h-8 w-8 text-white mr-1" />
+          <div>{{ uploadStatus }}</div>
+        </div>
+        <div v-else class="flex items-center">
+          <BadgeCheckIcon class="h-8 w-8 text-white mr-1" />
+          <div>{{ uploadStatus }}</div>
+        </div>
+      </div>
+    </div>
     <div class="bg-gray-900 p-4 text-white text-lg">
       <router-link to="/" class="flex items-center">
-        <ArrowLeftIcon class="h-5 w-5 text-white mr-1" /> Home</router-link
+        <ArrowLeftIcon class="h-5 w-5 text-white mr-1" /> Back</router-link
       >
     </div>
-    <div class="p-6 flex-grow">
+    <div class="flex-grow">
       <div
         v-if="!photoURL"
         class="flex justify-center items-center w-full h-full"
@@ -15,12 +47,11 @@
           class="
             h-full
             w-full
-            border-4 border-dashed border-black
-            rounded-xl
+            shadow-inner
+            bg-gray-300
             flex flex-col
             items-center
             justify-center
-            bg-white
           "
         >
           <div class="flex flex-col justify-center items-center pt-5 pb-6">
@@ -32,33 +63,60 @@
           <input
             id="image-file"
             type="file"
-            accept="image/*"
             capture="camera"
             class="hidden"
             @change="setImage"
           />
         </label>
       </div>
-      <div v-else class="w-full flex flex-col">
-        <img :src="photoURL" />
-        <button class="p-2 border border-gray-500 mt-2" @click="retakePhoto">
+      <div
+        v-else
+        class="
+          w-full
+          flex flex-col
+          justify-center
+          items-center
+          overflow-hidden
+          relative
+        "
+      >
+        <button
+          class="p-2 mt-2 bg-white w-full z-40 shadow-md"
+          @click="retakePhoto"
+        >
           Re-take Photo
         </button>
+        <div class="h-full">
+          <img :src="photoURL" class="w-[300px]" />
+        </div>
       </div>
     </div>
   </div>
-  <div class="bg-white h-1/2 p-6 flex flex-col justify-between">
-    <div class="flex flex-col h-full">
+  <div class="bg-white p-6 h-1/3 flex flex-col justify-between">
+    <div class="flex flex-col flex-grow">
       <div class="text-lg font-bold mt-2">Notes:</div>
       <div class="w-full h-full flex-grow">
         <textarea
           class="w-full border border-gray-500 p-2 h-5/6 rounded-sm resize-none"
           placeholder="describe this image"
+          v-model="photoNotes"
         ></textarea>
       </div>
     </div>
     <div class="w-full">
-      <button class="w-full border border-gray-500 p-4 text-xl rounded-sm">
+      <button
+        class="
+          w-full
+          border border-gray-500
+          bg-white
+          p-4
+          text-xl
+          rounded-sm
+          disabled:opacity-50
+        "
+        :disabled="photoURL == null ? true : false"
+        @click="submitPhoto"
+      >
         Submit
       </button>
     </div>
@@ -69,13 +127,23 @@
 import { onMounted, ref, watch } from "vue";
 import { defaultStore } from "../store";
 import { useRouter, useRoute } from "vue-router";
+import axios from "axios";
 
 import { ArrowLeftIcon } from "@heroicons/vue/solid";
 import { CameraIcon } from "@heroicons/vue/outline";
+import { BadgeCheckIcon } from "@heroicons/vue/outline";
+
+const store = defaultStore();
+const router = useRouter();
+const route = useRoute();
 
 const uploadTime = ref();
 const photoFile = ref();
 const photoURL = ref();
+const photoNotes = ref();
+const uploadSuccess = ref(false);
+const uploadStatus = ref("");
+const responsedata = ref();
 
 const setImage = (e) => {
   photoFile.value = e.target.files[0];
@@ -85,5 +153,63 @@ const retakePhoto = (e) => {
   photoURL.value = null;
 };
 
-console.log(new Date().toLocaleTimeString());
+const submitPhoto = async () => {
+  // var data = {
+  //   photo: photoFile.value,
+  //   url: photoURL.value,
+  // };
+  // const formData = getFormData(data);
+
+  let formData = new FormData();
+  formData.append("file", photoFile.value);
+  formData.append("url", photoURL.value);
+  formData.append("userName", store.userName);
+  formData.append("sessionName", store.sessionName);
+  formData.append("sessionNameNoSapce", store.sessionName.replace(/\s/g, ""));
+  formData.append("sessionNotes", store.sessionNotes);
+  formData.append("sessionDate", store.sessionDate);
+  formData.append("photoNotes", photoNotes.value);
+  formData.append("uploadTime", new Date().toLocaleTimeString());
+
+  const response = await axios
+    .post(
+      "https://www.fhistudio-apps.com/fieldphotos/php/upload.php",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    )
+    .then(function (response) {
+      responsedata.value = response.data;
+      if (!response.data) {
+        alert("File not uploaded.");
+        uploadSuccess.value = true;
+        uploadStatus.value = "Fail - Try Again";
+        setTimeout(() => {
+          uploadSuccess.value = false;
+          // photoURL.value = null;
+        }, 3000);
+      } else {
+        // alert("File uploaded successfully.");
+        uploadSuccess.value = true;
+        uploadStatus.value = "Success";
+        setTimeout(() => {
+          uploadSuccess.value = false;
+          photoURL.value = null;
+          photoNotes.value = "";
+        }, 3000);
+      }
+    })
+    .catch(function (error) {
+      responsedata.value = error;
+    });
+};
+
+const getFormData = (object) =>
+  Object.keys(object).reduce((formData, key) => {
+    formData.append(key, object[key]);
+    return formData;
+  }, new FormData());
 </script>
