ec6a1b691b6a61b270df5e81bbfaee28905a53a8
Eric <esmith@fhistudio.com>
2022-07-28 17:33:56 -0400

    layout and grid zooming

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -72,44 +72,79 @@
         class="w-full p-2 bg-gray-100 mt-1 flex items-center justify-between"
       >
         <div class="flex flex-grow md:flex-row flex-col items-center">
-          <div class="pl-4 pr-2 font-extrabold flex items-center">
-            <CalendarIcon class="mr-1 w-4 h-4 font-extrabold" />
-            Show all photos...
+          <div class="flex items-center">
+            <div class="pl-4 pr-2 font-extrabold flex items-center">
+              <CalendarIcon class="mr-1 w-4 h-4 font-extrabold" />
+              Show all photos...
+            </div>
+            <select
+              v-model="dateOperator"
+              class="
+                w-28
+                p-2
+                bg-transparent
+                focus:outline-none
+                outline-none
+                border-b-2 border-green-500
+                text-center
+              "
+              @change="updateDateOperator"
+            >
+              <option value="from">from</option>
+              <option value="before">before</option>
+              <option value="after">after</option>
+              <option value="between">between</option>
+            </select>
           </div>
-          <select
-            v-model="dateOperator"
-            class="
-              w-28
-              p-2
-              bg-transparent
-              focus:outline-none
-              outline-none
-              border-b-2 border-green-500
-              text-center
-            "
-            @change="updateDateOperator"
-          >
-            <option value="from">from</option>
-            <option value="before">before</option>
-            <option value="after">after</option>
-            <option value="between">between</option>
-          </select>
-          <input
-            type="date"
-            class="w-34 p-2 bg-transparent focus:outline-none outline-none"
-            v-model="startDate"
-          />
-          <div class="px-2 font-extrabold" v-if="dateOperator == 'between'">
-            and
+          <div class="flex items-center">
+            <input
+              type="date"
+              class="
+                w-36
+                h-10
+                p-2
+                md:mx-2
+                bg-transparent
+                focus:outline-none
+                outline-none
+                border-b-2 border-green-500
+              "
+              v-model="startDate"
+            />
+            <div class="px-2 font-extrabold" v-if="dateOperator == 'between'">
+              and
+            </div>
+            <input
+              v-if="dateOperator == 'between'"
+              type="date"
+              class="
+                w-36
+                h-10
+                p-2
+                md:mx-2
+                bg-transparent
+                focus:outline-none
+                outline-none
+                border-b-2 border-green-500
+              "
+              v-model="endDate"
+            />
           </div>
-          <input
-            v-if="dateOperator == 'between'"
-            type="date"
-            class="w-34 p-2 bg-transparent focus:outline-none outline-none"
-            v-model="endDate"
-          />
           <button
-            class="p-2 px-4 border border-gray-900 ml-4 flex items-center"
+            class="
+              p-2
+              px-4
+              border border-gray-900
+              ml-4
+              w-full
+              md:w-auto
+              flex
+              items-center
+              mt-2
+              md:mt-0
+              text-center
+              justify-center
+            "
             @click="loadByDate"
           >
             GO
@@ -146,7 +181,7 @@
           </Marker>
         </GoogleMap>
       </div>
-      <div class="md:w-1/2 w-full h-full p-2 overflow-scroll">
+      <div class="md:w-1/2 w-full h-full overflow-scroll">
         <div
           v-if="noPhotosFound == null"
           class="
@@ -161,10 +196,11 @@
             bg-clip-text bg-gradient-to-br
             from-blue-400
             to-green-600
+            text-center
           "
         >
           <SearchIcon class="mr-1 w-10 h-10 text-yellow-500" />
-          Search for a session above
+          Load photos using the tools above.
         </div>
         <div
           v-if="noPhotosFound == true"
@@ -189,33 +225,91 @@
           Refine your search.
         </div>
 
-        <div
-          v-if="noPhotosFound == false"
-          class="grid md:grid-cols-4 grid-cols-3 gap-2"
-        >
-          <div v-for="photo in photos" :key="photo.id">
-            <button
-              class="
-                col-span-1
-                h-40
-                overflow-hidden
-                flex
-                items-center
-                justify-center
-                bg-black
-                hover:scale-105 hover:shadow-md hover:shadow-gray-500
-                transition-all
-                duration-300
-              "
-              @click="zoomPhoto(photo)"
-            >
-              <img
-                :src="
-                  'https://www.fhistudio-apps.com/fieldphotos/uploads/' +
-                  photo.fileName
+        <div v-if="noPhotosFound == false">
+          <div
+            class="
+              bg-gray-100
+              w-full
+              sticky
+              top-0
+              shadow-md
+              flex
+              z-40
+              justify-between
+            "
+          >
+            <div class="flex items-center font-extrabold pl-4">
+              {{ photos.length }} photos
+            </div>
+            <div class="flex items-cetner">
+              <button
+                class="
+                  p-2
+                  bg-gray-200
+                  px-4
+                  mr-1
+                  hover:bg-gray-300
+                  transition-all
+                  duration-200
                 "
-              />
-            </button>
+                @click="changeGridClassExpand"
+              >
+                <ZoomOutIcon class="text-gray-700 mr-1 w-6 h-6" />
+              </button>
+              <button
+                class="
+                  p-2
+                  bg-gray-200
+                  px-4
+                  mr-1
+                  hover:bg-gray-300
+                  transition-all
+                  duration-200
+                "
+                @click="changeGridClassReset"
+              >
+                <SearchIcon class="text-gray-700 mr-1 w-6 h-6" />
+              </button>
+              <button
+                class="
+                  p-2
+                  bg-gray-200
+                  px-4
+                  hover:bg-gray-300
+                  transition-all
+                  duration-200
+                "
+                @click="changeGridClassShrink"
+              >
+                <ZoomInIcon class="text-gray-700 mr-1 w-6 h-6" />
+              </button>
+            </div>
+          </div>
+          <div class="grid gap-1 bg-gray-50 p-2" :class="gridClass">
+            <div v-for="photo in photos" :key="photo.id">
+              <button
+                class="
+                  col-span-1
+                  h-40
+                  overflow-hidden
+                  flex
+                  items-center
+                  justify-center
+                  bg-black
+                  hover:scale-105 hover:shadow-md hover:shadow-gray-500
+                  transition-all
+                  duration-300
+                "
+                @click="zoomPhoto(photo)"
+              >
+                <img
+                  :src="
+                    'https://www.fhistudio-apps.com/fieldphotos/uploads/' +
+                    photo.fileName
+                  "
+                />
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -223,7 +317,16 @@
 
     <div
       v-if="zoomShowing"
-      class="absolute bg-white/80 backdrop-blur-lg h-full w-full p-4"
+      class="
+        absolute
+        bg-white/80
+        backdrop-blur-lg
+        h-screen
+        w-full
+        p-4
+        overflow-scroll
+        z-50
+      "
     >
       <div class="w-full flex justify-end mb-2">
         <XIcon
@@ -238,7 +341,11 @@
             absolute
             p-2
             rounded-full
+            shadow-xl
             border border-green-600
+            hover:rotate-180
+            transition-all
+            duration-300
           "
           @click="zoomPhoto(null)"
         />
@@ -254,7 +361,7 @@
           />
         </div>
         <div class="md:w-1/2 p-4">
-          <div class="font-bold mt-2 text-2xl text-green-700">
+          <div class="font-bold md:mt-2 text-2xl text-green-700">
             Session Name:
           </div>
           <div class="italic text-xl">{{ selectedPhoto.sessionName }}</div>
@@ -296,6 +403,8 @@ import { MenuIcon } from "@heroicons/vue/outline";
 import { XIcon } from "@heroicons/vue/outline";
 import { SearchIcon } from "@heroicons/vue/outline";
 import { CalendarIcon } from "@heroicons/vue/outline";
+import { ZoomInIcon } from "@heroicons/vue/outline";
+import { ZoomOutIcon } from "@heroicons/vue/outline";
 
 const store = defaultStore();
 const router = useRouter();
@@ -310,10 +419,24 @@ const zoomShowing = ref(false);
 const dateFilterShowing = ref(false);
 const selectedPhoto = ref({});
 
-const dateOperator = ref("from");
-const startDate = ref("");
-const endDate = ref("");
+//SET TODAY//
+var date = new Date();
 
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+//SET TODAY//
+
+const dateOperator = ref("from");
+const startDate = ref(today);
+const endDate = ref(today);
+
+console.log(new Date().toLocaleDateString());
 //MAP
 const center = ref({ lat: 41.7679044, lng: -72.751958 });
 const zoom = ref(12);
@@ -321,10 +444,23 @@ const pk = import.meta.env.VITE_MAPKEY;
 
 const pLocation = ref({});
 
+//GRID
+const gridClass = ref("md:grid-cols-4 grid-cols-3");
+
 const emit = defineEmits(["showMenu"]);
 
 const showMenu = () => {
   emit("showMenu");
+};
+
+const changeGridClassExpand = () => {
+  gridClass.value = "md:grid-cols-5 grid-cols-4";
+};
+const changeGridClassShrink = () => {
+  gridClass.value = "md:grid-cols-3 grid-cols-2";
+};
+const changeGridClassReset = () => {
+  gridClass.value = "md:grid-cols-4 grid-cols-3";
 };
 
 const zoomPhoto = (photo) => {
@@ -444,7 +580,6 @@ const loadAll = async () => {
   photos.value = response.data;
 
   for (const [key, value] of Object.entries(response.data)) {
-
     var latitude = parseFloat(value.lat);
     var longitude = parseFloat(value.lng);
 
